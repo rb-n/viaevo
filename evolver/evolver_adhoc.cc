70db7306ed722136a2ea9da5b4cffe1abf041a70
Richard Baran <richard@baranbioscience.com>
2023-02-08 15:54:54 -1000

    Add a rudimentary EvolverAdHoc.

---
+++ evolver/evolver_adhoc.cc
@@ -0,0 +1,80 @@
+// Copyright (c) 2023 Richard Baran
+//
+// All components of viaevo are licensed under the MIT License.
+// See LICENSE.txt in the root of the repository.
+
+#include "evolver_adhoc.h"
+
+#include <algorithm>
+#include <memory>
+
+namespace viaevo {
+
+namespace {
+class ProgramCompare {
+public:
+  bool operator()(const std::shared_ptr<Program> &lhs,
+                  const std::shared_ptr<Program> &rhs) const {
+    return lhs->current_score() > rhs->current_score();
+  }
+};
+} // namespace
+
+EvolverAdHoc::EvolverAdHoc(int mu, int phi, int lambda, Scorer &scorer,
+                           Mutator &mutator, Random &gen, int max_generations)
+    : mu_(mu), phi_(phi), lambda_(lambda),
+      programs_(std::vector<std::shared_ptr<Program>>(
+          mu_ + lambda_, Program::CreateSimpleSmall())),
+      scorer_(scorer), mutator_(mutator), gen_(gen),
+      max_generations_(max_generations) {}
+
+// TODO: Put the three stages in individual member functions and add unit tests
+// for those.
+void EvolverAdHoc::Run() {
+  while (current_generation_ < max_generations_) {
+    ++current_generation_;
+
+    // Stage 1 (SelectParents): Bring the mu_ parents to the "front" of
+    // programs_.
+    // --------------------------------------------------------------
+    // Shuffle programs_ to prevent breaking ties the same way in each
+    // generation.
+    // TODO: Provide own random number generator.
+    std::random_shuffle(programs_.begin(), programs_.end());
+    // Bring the (mu_ - phi_) parents (selected on score) to the "front" of
+    // programs_.
+    std::nth_element(programs_.begin(), programs_.begin() + (mu_ - phi_) - 1,
+                     programs_.end(), ProgramCompare());
+    // Shuffle the elements after (mu_ - phi_) to obtain the remaining phi_
+    // parents at random.
+    // TODO: Provide own random number generator.
+    std::random_shuffle(programs_.begin() + (mu_ - phi_), programs_.end());
+
+    // Stage 2 (CreateOffspring): Create lambda_ offspring in the last lambda_
+    // elements of programs_ using the first mu_ elements of programs as
+    // parents.
+    // --------------------------------------------------------------------
+    for (int i = 0; i < lambda_; ++i) {
+      // Note: The same program may be selected as both parent1 and parent2 and
+      // this is ok for e.g. random recombinations.
+      mutator_.Mutate(programs_[mu_ + i], programs_[gen_() % mu_],
+                      programs_[gen_() % mu_]);
+    }
+
+    // Stage 3 (EvaluatePrograms): Update programs_ inputs, execute and score
+    // programs_.
+    // -----------------------------------------
+    // TODO: Enable multiple evaluation rounds (new inputs/execution/evaluation)
+    // per generation.
+    scorer_.ResetInputs();
+    for (int i = 0; i < mu_ + lambda_; ++i) {
+      programs_[i]->ResetCurrentScore();
+      programs_[i]->SetElfInputs(scorer_.current_inputs());
+      programs_[i]->Execute();
+      programs_[i]->IncrementCurrentScoreBy(
+          scorer_.Score(programs_[i]->last_results()));
+    }
+  }
+}
+
+} // namespace viaevo
---
+++ evolver/evolver_adhoc.h
@@ -0,0 +1,64 @@
+// Copyright (c) 2023 Richard Baran
+//
+// All components of viaevo are licensed under the MIT License.
+// See LICENSE.txt in the root of the repository.
+
+#ifndef VIAEVO_EVOLVER_EVOLVER_ADHOC_H_
+#define VIAEVO_EVOLVER_EVOLVER_ADHOC_H_
+
+#include <memory>
+#include <vector>
+
+// TODO: Remove relative paths.
+#include "../mutator/mutator.h"
+#include "../program/program.h"
+#include "../scorer/scorer.h"
+#include "../util/random.h"
+
+namespace viaevo {
+
+// Evolver manages a population of Programs and rounds of mutation,
+// evaluation, and selection. EvolverAdHoc's implementation is inspired by
+// Genetic Programming and (mu + lambda) Evolution Strategy (with stochastic
+// ranking). The current implementation probably does not match any of these
+// exactly. Eventually, it would be worthwhile to create an abstract base class
+// Evolver specifying a generic interface and a set of concrete derived classes
+// reflecting common approaches in Evolutionary Computation.
+class EvolverAdHoc {
+public:
+  EvolverAdHoc(int mu, int phi, int lambda, Scorer &scorer, Mutator &mutator, Random &gen,
+               int max_generations);
+  // Runs the evolution.
+  virtual void Run();
+
+  const std::vector<std::shared_ptr<Program>> &programs() { return programs_; };
+
+protected:
+  // Size of population in each generation (iteration) is (mu_ + lambda_).
+  // Number of parents selected in each iteration.
+  int mu_ = 25;
+  // Number of parents selected randomly in each iterations as opposed to (mu_ -
+  // phi_) parents that are selected based on their scores. mu_ >= phi_;
+  int phi_ = 5;
+  // Number of offspring created from mu_ parents in each iteration.
+  int lambda_ = 70;
+
+  // Population of Programs.
+  std::vector<std::shared_ptr<Program>> programs_;
+
+  // Scorer used to provide input data and score results in each iteration.
+  Scorer &scorer_;
+  // Mutator used to create offspring from parents in each iteration.
+  Mutator &mutator_;
+  // Random number generator.
+  Random &gen_;
+
+  // Current and the maximum number of generations (iterations) for the
+  // evolution.
+  int current_generation_ = 0;
+  int max_generations_ = 10'000;
+};
+
+} // namespace viaevo
+
+#endif // VIAEVO_EVOLVER_EVOLVER_ADHOC_H_
