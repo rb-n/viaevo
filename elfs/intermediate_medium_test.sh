8cad837940e3765d499cc2372cf016c8655f4ee3
Richard Baran <richard@baranbioscience.com>
2023-03-29 15:16:13 -1000

    Add intermediate_medium elf.

---
+++ elfs/intermediate_medium_test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Only tests for a successful execution of the program (e.g. without segfault)
+# and an exit code of 0. More testing done in //program:program_test.cc.
+elfs/intermediate_medium
